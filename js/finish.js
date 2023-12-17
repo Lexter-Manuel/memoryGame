c91a927f262c6c7375d72d85300a1f17c996d275
Lexter-Manuel <lextermanuel.neust@gmail.com>
2023-12-17 11:06:02 +0800

    memoryGame

---
+++ js/finish.js
@@ -0,0 +1,9 @@
+function finish(){
+    if(levelActual < level.length - 1){
+       document.querySelector("#levelUp").classList.add("visible"); 
+    }else{
+        document.querySelector("#endGame").classList.add("visible");
+    }
+    
+}
+
